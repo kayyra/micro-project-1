c0abb9c07326c8aac9a795d3af1552041be0d715
Ana <anarodriguez.p@hotmail.es>
2023-12-01 10:16:50 +0100

    finished

--- src/components/ContadorClase.jsx
+++ src/components/ContadorClase.jsx
@@ -1,40 +1,28 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 
+//Componente basado en una función. 
+const ContadorClase = () =>{
+    
+    //Aquí definimos cual es el valor de este conteo, además de que con el useState estamos generando un valor por defecto. 
+    const [conteo, setConteo] = useState(0);
 
-class ContadorClase extends Component{
- 
-    constructor(props){
-        super(props);
-        this.state ={
-            conteo: 0,
-        };
-    }
-
-    incrementar = () => {
-        this.setState((prevState) => ({ conteo: prevState.conteo +1}));
+    const incrementar = () => {
+        setConteo((prevConteo) => prevConteo +1);
     };
 
-    reducir = () =>{
-        this.setState((prevState) => ({ conteo: prevState.conteo -1}));
-    }
+    const reducir = () =>{
+        setConteo((prevConteo) => prevConteo -1);
+    };
 
-    render (){
-        
-        const { conteo } = this.state;
-
-        return(
-           
+        return(           
             <div>
-                <h1 text='Contador de Cliks' />   
-                <p style={{marginLeft: 60, fontSize: 50, textAlign: "center"}}>Conteo: {conteo} </p>
-                <button onClick={this.incrementar} style={{marginRight:30, marginLeft:830,  fontSize: 22}} >Sumar Clicks</button>  
-                <button onClick={this.reducir} style={{fontSize: 22}} >Reducir Cliks</button>   
+                <h1 style={{textAlign: "center"}}>Contador de Cliks</h1>   
+                <p style={{fontSize: 20, textAlign: "center"}}>Conteo: {conteo} </p>                 
+                <button variant="text" onClick={incrementar} style={{marginRight:30, marginLeft: 840, fontSize: 15}} >Sumar Clicks</button>  
+                <button onClick={reducir} style={{fontSize: 15}} >Reducir Cliks</button>  
             </div>
-            
         );
     }
-    
-}
 
 export default ContadorClase
 
